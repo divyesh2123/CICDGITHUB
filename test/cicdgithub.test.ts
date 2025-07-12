dccc01d7c921c6b75ce8c652cf5ddc673c8ba5dc
divyesh2123 <divyesh2198@gmail.com>
2025-07-12 15:03:44 +0530

    dsf

---
+++ test/cicdgithub.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cicdgithub from '../lib/cicdgithub-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicdgithub-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cicdgithub.CicdgithubStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
